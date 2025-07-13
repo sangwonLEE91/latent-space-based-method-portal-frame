75d6d2b745aa9c96098d1b807a0753ac938191a1
lee <lee-sangwon@g.ecc.u-tokyo.ac.jp>
2025-07-13 22:23:48 +0900

    Auto commit at 2025-07-13 22:23:48

--- Model_updating_main.py
+++ Model_updating_main.py
@@ -14,8 +14,8 @@ from scipy.stats import uniform
 
 test = 1
 
-N0 = 8000  # Number of training samples
-Nv0 = 2000  # Number of validation samples
+N0 = 80  # Number of training samples
+Nv0 = 20  # Number of validation samples
 
 learning_rate = 0.0001  # Learning rate for the VAE training
 train_patience = 500  # Patience for stopping during VAE training
@@ -40,8 +40,6 @@ print(device)
 true_theta = np.array([3, 3 + np.log10(5), np.log10(2)]) # Ground truth parameters for the portal frame model
 y_obs = np.load('observation/response_obs.npy')
 print(y_obs.shape)
-channel = 5
-exit()
 
 ### Priors based on the case
 prior1 = uniform(1, 4)
@@ -50,7 +48,7 @@ prior3 = uniform(0, 1)
 prior = [prior1, prior2, prior3]
 prior_pdf = lambda theta: prior_p(theta, prior=prior)
 range_theta = [[1, 1, 0], [4, 4, 1]]
-prior_rnd = lambda n_samples: LHS(bounds=range_theta, n_samples=n_samples)
+prior_rnd = lambda n_samples: LHS(n_samples, bounds=range_theta)
 
 
 ### Constants  for TMCMC
@@ -77,7 +75,6 @@ taper = taper_maker(dt, 1, ndata)
 sim = lambda obs: multi_sim(obs, taper=taper, time_series=time_series, excitation=excitation, kmin=kmin, nstep=nstep,
                             add_noise=True)
 
-
 ### Sample from the prior distribution
 samples['pop'][j - 1] = prior_rnd(Ns)  # initial MCMC samples
 
@@ -95,7 +92,7 @@ Ncall = N0 + Nv0
 Ndim = samples['pop'][j - 1].shape[1]  # number of dimensions
 
 ### Training of VAE
-net = VAE(z_dim, channel, nstep, device).to(device)
+net = VAE(z_dim, y_obs.shape[1], nstep, device).to(device)
 save_torch = f'torch_model/t{test}.pth'
 if trained_model != True: # If you want to train a new model
     train_vae(net, Dataset, device, save_torch, train_patience, LR=learning_rate, valid_dataset=Dataset_val)
